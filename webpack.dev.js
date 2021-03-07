ababff0db08d61128dbb538f49b2a10b2db3a727
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-03-07 12:09:15 +0000

    Fix styles and js functionality
    Added .babelrc:
    - include the babel presets
    
    Updated .gitignore:
    - include dist folder
    
    Updated src/client/index:
    - imported scss files
    - commented out console.logs(checkForName)
    - commented out alert("I EXIST")
    - commented out alert("CHANGE!")
    - export checkForName
    - export handleSubmit
    
    Updated formHandler.js:
    - get checForName from Client library
    - update fetch to port 8081
    
    Updated style files:
    - all css files now scss files
    
    Updated src/server/index.js:
    - created port variable
    - updated listen function to use port variable
    
    Updated webpack.dev.js:
    - included rules for scss
    
    Updated webpack.prod.js:
    - included rules for scss

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,33 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    mode: "development",
-    devtool: "source-map",
-    entry: './src/client/index.js',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/client/index.js",
+  output: {
+    libraryTarget: "var",
+    library: "Client"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
 }
