008e22c4409393c8040ba62378c70524d8d14137
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-03-08 14:02:28 +0000

    Setup test get request
    Installed dependencies:
    - installed body-parser middleware
    - installed cors
    
    Updated src/server/index.js:
    - called and used body-parser
    - called and used cors
    - re-wrote all functions as arrow functions

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,31 @@
 var path = require("path")
 const express = require("express")
+const bodyParser = require("body-parser")
 const mockAPIResponse = require("./mockAPI.js")
 
 // meaningcloud api
 const dotenv = require("dotenv")
 dotenv.config()
 
+// use express
 const app = express()
 
+// cors enables scripts running on a browser client to interact with resources from a different origin
+const cors = require("cors")
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static("dist"))
 
 console.log(__dirname)
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile("dist/index.html")
+})
+
+// get route setup
+app.get("/test", (req, res) => {
+  res.send(mockAPIResponse)
 })
 
 // test env connection
@@ -22,10 +34,10 @@ console.log(`Your API key is ${process.env.API_KEY}`)
 const port = 8081
 
 // designates what port the app will listen to for incoming requests
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
-app.get("/test", function (req, res) {
+app.get("/test", (req, res) => {
   res.send(mockAPIResponse)
 })
