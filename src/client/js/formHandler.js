5b8ef0ec9e1e133e6a7a763c1ed922862bf44c8a
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-03-10 11:10:16 +0000

    Update front end
    Update formHandler.js:
    - added variables targeting span within paragraph tags of the #results div
    - set postData function to update innerText of variables with Meaning Cloud response output
    
    Removed nameChecker.js
    
    Updated index.html:
    - updated button value to 'Review'
    - created article review fields based on Meaning Cloud response output

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,9 @@
+const score = document.querySelector(".score span")
+const agreement = document.querySelector(".agreement span")
+const subjectivity = document.querySelector(".subjectivity span")
+const confidence = document.querySelector(".confidence span")
+const irony = document.querySelector(".irony span")
+
 function handleSubmit(event) {
   event.preventDefault()
 
@@ -6,6 +12,12 @@ function handleSubmit(event) {
   return Client.urlValid(formText)
     ? postData("http://localhost:8081/api", { url: formText }).then(res => {
         console.log("Form text submitted")
+
+        score.innerText = res.score_tag
+        agreement.innerText = res.agreement
+        subjectivity.innerText = res.subjectivity
+        confidence.innerText = res.subjectivity
+        irony.innerText = res.irony
       })
     : alert("URL invalid, please submit a valid link")
 }
