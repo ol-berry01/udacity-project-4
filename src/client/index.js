c92f4404e8eaf22c09f8373b56b7031a0d090ce3
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-03-11 13:03:07 +0000

    Update styles and general clean up
    Added img/coding-bg.jpg
    
    Updated index.js:
    - removed all existing style imports
    - added import for style.scss
    - removed all commented out code
    
    Renamed all existing scss sheets to have _ before the name
    
    Created style.scss:
    - imported _variables.scss
    - imported _resets.scss
    - imported _base.scss
    - imported _footer.scss
    - imported _form.scss
    - imported _header.scss
    
    Created variables.scss:
    - set $primary equal to dodgerblue
    - set $secondary equal to #333
    - set $background equal to #f4f4f4
    
    Updated index.html:
    - removed links to css files
    - added meta viewport
    - renamed the title of the doc to OWB Sentiment Analyser
    - updated the header content
    - added classes to each of the section tags
    - restructured the results section
    - added new content to the footer, including author, API and background photo information

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,7 @@
-import "./styles/resets.scss"
-import "./styles/base.scss"
-import "./styles/footer.scss"
-import "./styles/form.scss"
-import "./styles/header.scss"
+import "./styles/style.scss"
 
-// import { checkForName } from "./js/nameChecker"
 import { urlValid } from "./js/urlValid"
 import { scoreChecker } from "./js/scoreChecker"
 import { handleSubmit } from "./js/formHandler"
-
-// console.log(checkForName)
-
-// alert("I EXIST")
-// console.log("CHANGE!!")
 
 export { urlValid, scoreChecker, handleSubmit }
